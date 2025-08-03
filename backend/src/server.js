9004210572db98879e0a92dc240178390bba70a6
Snigdh <snigdh27@gmail.com>
2025-08-04 00:04:44 +0530

    code structure refactored and db connection made

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import "dotenv/config";
+const app = express();
+const PORT = process.env.PORT;
+import authRoutes from "../src/routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
