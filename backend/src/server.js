b53b02617a7dc2a89a178dcdb33b7fd7e7c62698
Snigdh <snigdh27@gmail.com>
2025-08-06 00:21:56 +0530

    method created for signup, login and logout

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT;
 import authRoutes from "../src/routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
