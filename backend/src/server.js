3da734da31d3b1005eae994332113f7ea6509e4d
Snigdh <snigdh27@gmail.com>
2025-08-10 23:03:18 +0530

    friend request api added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,15 @@ import express from "express";
 import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT;
+import cookieParser from "cookie-parser";
 import authRoutes from "../src/routes/auth.route.js";
+import userRoutes from "../src/routes/user.route.js";
 import { connectDB } from "./lib/db.js";
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
